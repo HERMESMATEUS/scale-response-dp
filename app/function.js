b4e53a222b3e45e4469b22e597b2bc86d17a3373
Administrador <administrador@DESARROLLO.local>
2019-12-16 17:21:49 -0500

    documentation 2

--- app/function.js
+++ app/function.js
@@ -1,5 +1,4 @@
 import { Dimensions, PixelRatio, StatusBar } from 'react-native';
-
 // Este ancho es el que establece la parte de diseño y la escala que se va a usar para calcular el valor de un pixel a un dp
 const DPWidth = 360;
 // Este alto es el que establece la parte de diseño y la escala que se va a usar para calcular el valor de un pixel a un dp
@@ -12,9 +11,13 @@ export function FontSizeRP(size) { return size * PixelRatio.getFontScale() }
 /* 
 Esta funcion obtenemos el alto de la pantalla con "Dimensions.get('screen').height" y le restamos el tamaño 
 de la barra de accion de android con "StatusBar.currentHeight" seguido a eso lo dividimos segun la escala 
-que tenemos del ancho "DPHeight" para obtener el valor de en pixeles y el valor de es dp equivalente ya en pixeles
+que tenemos del ALTO "DPHeight" para obtener el valor de en pixeles y el valor de es dp equivalente ya en pixeles
 lo multiplicamos por el dp de diseño
 */
 export function getPixelByHeightDP(dp) { return (dp * ((Dimensions.get('screen').height - StatusBar.currentHeight) / DPHeight)) }
-
+/*
+Esta funcion obtenemos el ANCHO de la pantalla con "Dimensions.get('screen').width" lo dividimos segun la escala 
+que tenemos del ancho "DPWidth" para obtener el valor de en pixeles y el valor de es dp equivalente ya en pixeles
+lo multiplicamos por el dp de diseño
+*/
 export function getPixelByWidthDP(dp) { return (dp * (Dimensions.get('screen').width / DPWidth)) }
